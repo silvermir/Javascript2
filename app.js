3153a626d27787864cf733e2e95119e354d4b550
Ugur Erdemir <ugur.erdemir@mercedes-benz.com>
2022-05-21 10:14:47 +0200

    added Creature Constructor

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-
-    // Create Dino Constructor
-
-
-    // Create Dino Objects
-
-
-    // Create Human Object
-
-    // Use IIFE to get human data from form
-
-
-    // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
-
-    
-    // Create Dino Compare Method 2
-    // NOTE: Weight in JSON file is in lbs, height in inches.
-
-    
-    // Create Dino Compare Method 3
-    // NOTE: Weight in JSON file is in lbs, height in inches.
+// Create Dino Constructor
+function Creature(species, weight, height, diet, where, when, fact, name) {
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+    this.name = name;
+}
 
 
-    // Generate Tiles for each Dino in Array
-  
-        // Add tiles to DOM
+// Create Dino Objects
 
-    // Remove form from screen
+
+// Create Human Object
+
+// Use IIFE to get human data from form
+
+
+// Create Dino Compare Method 1
+// NOTE: Weight in JSON file is in lbs, height in inches. 
+
+
+// Create Dino Compare Method 2
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Create Dino Compare Method 3
+// NOTE: Weight in JSON file is in lbs, height in inches.
+
+
+// Generate Tiles for each Dino in Array
+
+// Add tiles to DOM
+
+// Remove form from screen
 
 
 // On button click, prepare and display infographic
