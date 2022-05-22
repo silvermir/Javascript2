59cea9f9b66a23b7f28bd7af445874d274a2e36f
Ugur Erdemir <ugur.erdemir@mercedes-benz.com>
2022-05-22 09:01:16 +0200

    app.js: - added dinos data from json - added Human Data from Form - Created dinos Objects - created human Object
    dino.json:
    - changed Brachiosaurus height value from string in to number

--- app.js
+++ app.js
@@ -12,11 +12,51 @@ function Creature(species, weight, height, diet, where, when, fact, name) {
 
 
 // Create Dino Objects
+let dinos = [];
 
+function createDino() {
+    return fetch("dino.json")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function({ Dinos }) {
+            Dinos.map(function(dinosaurs) {
+                let dino = new Creature(...Object.values(dinosaurs));
+                dinos.push(dino)
+            })
+        })
+};
+
+createDino();
 
 // Create Human Object
 
+let human = [];
+console.log(human)
+
+function createHuman(formHumanData) {
+    formHumanData.map(function(humanData) {
+        let creatureHuman = new Creature(...Object.values(humanData));
+        human.push(creatureHuman)
+    })
+};
+
 // Use IIFE to get human data from form
+document.getElementById("btn").addEventListener("click", function() {
+    (function formHumanData() {
+        let humanData = [{
+            species: "Human",
+            weight: document.querySelector("#weight").value,
+            height: document.querySelector("#feet").value * 12 || document.querySelector("#inches").value,
+            diet: document.querySelector("#diet").value,
+            where: "",
+            when: "",
+            fact: "",
+            name: document.querySelector("#name").value
+        }];
+        createHuman(humanData);
+    })();
+});
 
 
 // Create Dino Compare Method 1
