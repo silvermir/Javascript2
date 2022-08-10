ff2ac9dd4b4415b5e89164418cc72facffc6502d
Ugur Erdemir <ugur.erdemir@mercedes-benz.com>
2022-08-10 20:37:25 +0200

    added Nasa API  (Manifest, Photos)

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -4,18 +4,17 @@ const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
 const path = require('path')
 
-const app = express()
+const server = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+server.use(bodyParser.urlencoded({ extended: false }))
+server.use(bodyParser.json())
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+server.use('/', express.static(path.join(__dirname, '../public')))
 
-// your API calls
 
-// example API call
-app.get('/apod', async (req, res) => {
+// Apod API call
+server.get('/apod', async(req, res) => {
     try {
         let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
             .then(res => res.json())
@@ -25,4 +24,28 @@ app.get('/apod', async (req, res) => {
     }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//Manifest API call 
+server.get('/roverData/:rover', async(req, res) => {
+    let rover = req.params.rover
+    try {
+        let data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ data })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+//Photos API call 
+server.get('/roverPhotos/:rover', async(req, res) => {
+    let rover = req.params.rover
+    try {
+        let photos = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latsest_photos?api_key=${process.env.API_KEY}`)
+            .then(res => res.json())
+        res.send({ photos })
+    } catch (err) {
+        console.log('error:', err);
+    }
+})
+
+server.listen(port, () => console.log(`server listening on port ${port}!`))
